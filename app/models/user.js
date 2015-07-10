7b04d65812aea2b96e74a4c5e69f972cecb3296d
Kacper Goliński <kacper.golinski@netguru.co>
2015-07-10 20:19:37 +0200

    add user validations

--- app/models/user.js
+++ app/models/user.js
@@ -2,5 +2,19 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  password: DS.attr('strnig')
+  password: DS.attr('strnig'),
+
+  validations: {
+    email: {
+      presence: true,
+      format: {
+        with: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        message: 'your email is invalid'
+      }
+    },
+    password: {
+      presence: true,
+      length: { minimum: 6 }
+    }
+  }
 });
