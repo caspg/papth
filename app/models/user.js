322435bce3f64c7ce570d55ea69a00670a044da4
Kacper Goliński <kacper.golinski@netguru.co>
2015-07-10 20:30:00 +0200

    fix validation

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import EmberValidations from 'ember-validations';
 
-export default DS.Model.extend({
+export default DS.Model.extend(EmberValidations, {
   email: DS.attr('string'),
   password: DS.attr('strnig'),
 
