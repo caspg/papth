144d72c0a6e0ca1b4d4a77970cb78e01902692d4
Kacper Goliński <kacper.golinski@netguru.co>
2015-07-10 20:17:42 +0200

    add basic register logic

--- app/pods/landing-page/route.js
+++ app/pods/landing-page/route.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
+import Firebase from 'firebase';
+import config from 'papth/config/environment';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord("user");
+  },
+
+  actions: {
+    saveUser() {
+      var model = this.modelFor(this.routeName);
+      var ref = new Firebase(config.firebase);
+      ref.createUser({
+        email: model.get("email"),
+        password: model.get("password")
+      }, (error, userData) => {
+        if (error) {
+          console.log(error.message);
+        } else {
+          console.log("User was successfully created yo.")
+        }
+      })
+    }
+  }
 });
