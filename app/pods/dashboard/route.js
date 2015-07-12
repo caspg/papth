565d220a55b9ef6d91cf226a0cac2fa7ca56be92
Kacper Goliński <kacper.golinski@netguru.co>
2015-07-12 20:24:44 +0200

    use authenticated route mixin

--- app/pods/dashboard/route.js
+++ app/pods/dashboard/route.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   setupController(controller, model) {
     this._super(controller, model);
     controller.set("isNewForm", false)
