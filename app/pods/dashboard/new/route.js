efc653b769fa90e67c6ddc269f2a8f6e02e782dc
Kacper Goliński <kacper.golinski@netguru.co>
2015-07-12 20:54:25 +0200

    create new thougth form

--- app/pods/dashboard/new/route.js
+++ app/pods/dashboard/new/route.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    this.store.createRecord("thought");
+  },
+
   activate: function() {
-    this.controllerFor('dashboard').set('isNewForm', true);
+    this.controllerFor("dashboard").set("isNewForm", true);
   },
   deactivate: function() {
-    this.controllerFor('dashboard').set('isNewForm', false);
+    this.controllerFor("dashboard").set("isNewForm", false);
   }
 });
